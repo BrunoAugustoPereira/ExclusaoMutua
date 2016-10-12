6290144c20bfae0ec5505f88b074821d8e76234b
BrunoAugustoPereira <bruno.augusto.pp@gmail.com>
2016-10-12 19:35:32 -0300

    Update Cliente.java

--- ExclusaoMutua/src/Cliente.java
+++ ExclusaoMutua/src/Cliente.java
@@ -5,6 +5,7 @@ import java.io.*;
 public class Cliente{
 
     private Cliente() {}
+    public String ID;
 
     public static void main(String[] args) {
 
@@ -14,12 +15,13 @@ public class Cliente{
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             Registry registry = LocateRegistry.getRegistry(host, 1099); //host, port
             RecursoComp stub = (RecursoComp) registry.lookup("RecursoComp");
-
+            System.out.println("entre com o Identificador para esse processo");
+            String ID = br.readLine();
             System.out.println("[1] solicitar recurso [2] ver fila [3] ver status do recurso [x] sair");
             while(looping) {
                 String key = br.readLine();
                 try {
-                    stub.operation(key); //id + string, como a gente consegue o id?
+                    stub.operation(key,ID); //id + string, como a gente consegue o id?
                 } catch (Exception e) {
                 System.err. println("Client exception: " + e.toString());
                 e.printStackTrace();
