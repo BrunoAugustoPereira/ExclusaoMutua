ea966a33ba2293ccfbb8c969bcd7cf33611eabe1
BrunoAugustoPereira <bruno.augusto.pp@gmail.com>
2016-10-12 17:47:24 -0300

    Update Server.java

--- ExclusaoMutua/src/Server.java
+++ ExclusaoMutua/src/Server.java
@@ -16,13 +16,28 @@ public class Server implements RecursoComp {
     public Server() {}
 
     	public void requestCriticalZone(String id){
+		System.out.println("Acesso a Regiao critica solicitado pelo processo: "+ id);
 		if(regiaoOcupada == false) {
-			ocupante = fila_de_requisicoes.getFirst();
-			fila_de_requisicoes.remove(); //deleta o primeiro da fila
+			if (fila_de_requisicoes.size()>0){
+				fila_de_requisicoes.add(id);
+				ocupante = fila_de_requisicoes.getFirst();
+				fila_de_requisicoes.remove(); //deleta o primeiro da fila
+				regiaoOcupada = true;
+				System.out.println("Acesso a Regiao critica negado ao processo: "+ id+" adicionado a fila de requisicoes");
+				System.out.println("Acesso a Regiao critica concedido ao processo: "+ ocupante);
+			}
+			else if (fila_de_requisicoes.size()==0){
+			ocupante = id;
 			regiaoOcupada = true;
+			System.out.println("Acesso a Regiao critica concedido ao processo: "+ id);
+			}
+			
+			
+			
 		}
 		else if(regiaoOcupada == true) {
 			fila_de_requisicoes.add(id);
+			System.out.println("Acesso a Regiao critica negado ao processo: "+ id+" adicionado a fila de requisicoes");
 		}
 	}
 	
