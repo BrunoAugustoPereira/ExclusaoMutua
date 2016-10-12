f8f09d82d2f7c041370140e36a26e0ea9fd4236a
BrunoAugustoPereira <bruno.augusto.pp@gmail.com>
2016-10-11 23:00:56 -0300

    Update Cliente.java

--- ExclusaoMutua/src/Cliente.java
+++ ExclusaoMutua/src/Cliente.java
@@ -5,23 +5,37 @@ import java.io.*;
 public class Cliente extends Thread{
 
     private Cliente() {}
-
+    public void Run(){
+    
+    
+    }
     public static void main(String[] args) {
 
         String host = (args.length < 1) ? null : args[0];
         try {
         	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             Registry registry = LocateRegistry.getRegistry(host, 1099); //host, port
-            Calculator stub = (Calculator) registry.lookup("Calculator");
+            RecursoComp stub = (RecursoComp) registry.lookup("RecursoComp");
 
-            System.out.println("a ? ");
+            
+            
+            
+            System.out.println("[1] solicitar recurso [2] ver fila [3] ver status do recurso [0]sair ");
             int a = Integer.parseInt(br.readLine());
-            System.out.println("b ? ");
-            int b = Integer.parseInt(br.readLine());
-            System.out.println("operator ? ");
-            String operator = br.readLine();
-            int response = stub.op(a, b, operator);
-            System.out.println("response: " + response);
+            switch(a){
+                 case '1':
+                    //chama a solicatacao de recurso
+                    // stub.requestCriticalZone(## id do processo##);
+            
+                case '2':
+                    //chama a solicatacao de recurso
+                    // stub.displayQueue(); 
+                
+                case '3':
+                    //chama a solicatacao de recurso
+                    // stub.displayStatus(); 
+            }
+               
         } catch (Exception e) {
             System.err.	println("Client exception: " + e.toString());
             e.printStackTrace();
