326df7da7e78f003650abb2a162e5b06b3b99b73
BrunoAugustoPereira <bruno.augusto.pp@gmail.com>
2016-10-11 22:41:04 -0300

    Update RecursoComp.java

--- ExclusaoMutua/src/RecursoComp.java
+++ ExclusaoMutua/src/RecursoComp.java
@@ -2,5 +2,9 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface RecursoComp extends Remote {
+  
+  public void requestCriticalZone(int id) throws RemoteException;
+  public void displayQueue() throws RemoteException;
+  public void displayStatus() throws RemoteException;
 
 }
